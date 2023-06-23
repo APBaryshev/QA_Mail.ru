1d254a024eee81d43b175b962e4d3458a96672fe
APBaryshev <a.p.baryshev@gmail.com>
2023-06-23 15:36:35 +0300

    add correct test2

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -5,27 +5,30 @@ import { test, expect, Page } from '@playwright/test';
 test.describe('Email', () => {
 	test('check open form login and input', async ({ page }) => {
 		const btnComeIn1 = page.getByTestId('mailbox').getByRole('button', { name: 'Войти' });
-    const formLogin = page.locator("//h3[text()='Войти в аккаунт']");
-    const inputFormLogin = page.locator("//input[@placeholder='Имя аккаунта']");
-    const btnComeInFormLogin = page.locator("//span[text()='Ввести пароль']");
-    const headerFormPassword = page.locator("//h3[text()='Введите пароль']");
-		const formPassword = page.locator("//input[@type='password']");
-		const btnComeIn2 = page.getByTestId('submit-button').getByRole('button', { name: 'Войти' });
+    //const formLogin = page.locator("//h3[text()='Войти в аккаунт']");
+		//const formLogin = page.getByRole('heading', { name: 'Войти в аккаунт' });
+    //const inputFormLogin = page.locator("//input[@placeholder='Имя аккаунта']");
+		const inputFormLogin = page.frameLocator('.ag-popup__frame__layout__iframe').getByPlaceholder('Имя аккаунта');
+    //const btnComeInFormLogin = page.locator("//span[text()='Ввести пароль']");
+		const btnComeInFormLogin = page.frameLocator('.ag-popup__frame__layout__iframe').locator('[data-test-id="next-button"]');
+    //const headerFormPassword = page.locator("//h3[text()='Введите пароль']");
+		const headerFormPassword = page.getByRole('heading', { name: 'Введите пароль' });
+		//const formPassword = page.locator("//input[@type='password']");
+		const formPassword = page.frameLocator('.ag-popup__frame__layout__iframe').getByPlaceholder('Пароль');
+		const btnComeIn2 = page.frameLocator('.ag-popup__frame__layout__iframe').locator('[data-test-id="submit-button"]');
     
 		await page.goto('https://mail.ru/');
     await btnComeIn1.waitFor({ state: 'visible' });
 		expect (await btnComeIn1.isVisible()).toBe(true);
 		await btnComeIn1.click();
-		await page.screenshot({ path: 'screenshots/sreen.png' });
-		await page.waitForSelector()
-    await formLogin.waitFor({ state: 'visible' });
-    expect (await formLogin.isVisible()).toBe(true);
-		await inputFormLogin.fill('');
-		await page.locator("//span[text()='@mail.ru']").isVisible();
+		await inputFormLogin.click();
+		await inputFormLogin.fill('misterbinqa');
+		//await page.locator("//span[text()='@mail.ru']").isVisible();
     await btnComeInFormLogin.click();
-    await headerFormPassword.waitFor({ state: 'visible' });
-    await formPassword.fill('');
+    await formPassword.click();
+		await formPassword.fill('privetmrbin');
 		await btnComeIn2.click();
+		//await page.getByTestId('whiteline-account-exit').click();
 	
 });
 });
